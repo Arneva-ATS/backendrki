e1782cfd718bb07581b5b84f6bfb8c27b6004fb2
aditbest5 <44321384+aditbest5@users.noreply.github.com>
2024-09-15 17:33:47 +0700

    test

--- app/Http/Controllers/FelloController.php
+++ app/Http/Controllers/FelloController.php
@@ -11,7 +11,7 @@ class FelloController extends Controller
 {
     public function run(Request $request)
     {
-        $host = "http://35.187.249.186/";
+        $host = "http://34.116.84.239/";
 
         $cmd = $request->get("cmd");
         if ($cmd == "selMasterFello") {
